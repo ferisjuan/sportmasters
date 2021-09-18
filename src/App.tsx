6c1bf4bdf51eada88ca6f246e90ac9d9310957f5
Alejandro <aienma325@gmail.com>
2021-09-17 23:33:14 -0500

    Solved comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { ReactElement } from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { queryConfig } from './constants/queries'
-import { Dogs } from './pages/user'
+import { User } from './pages/user'
 
 const queryClient = new QueryClient(queryConfig)
 
-function App(): ReactElement {
+function Main(): ReactElement {
     return (
         <div className="App">
             <QueryClientProvider client={queryClient}>
-                <Dogs />
+                <User />
             </QueryClientProvider>
         </div>
     )
 }
 
-export default App
+export default Main
--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -1,9 +1,10 @@
 import { ReactElement } from 'react'
 import { useQuery } from 'react-query'
-import axios from 'axios'
 
-export const Dogs = (): ReactElement => {
-    const { data, error, isLoading } = useQuery('dogs', () => axios('https://dog.ceo/api/breeds/image/random'))
+import { http } from '../queries/axios'
+
+export const User = (): ReactElement => {
+    const { data, error, isLoading } = useQuery('dogs', () => http('/image/random'))
 
     if (error) return <h1>Error: {error}, try again</h1>
     if (isLoading) return <h1>Loading...</h1>
