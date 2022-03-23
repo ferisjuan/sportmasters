0e757358416a1594469d93773e0e0b092cce8b15
Juan Feris <feris.juan@gmail.com>
2022-03-23 15:56:04 -0500

    feat(fe): vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '',
+  root: '.',
+  build: {
+    emptyOutDir: true,
+    outDir: "build"
+  },
   plugins: [react(), tsconfigPaths()]
 })
