13e26ada148e9ca1ca2d046e519fa58bb3f33eac
Juan Feris <feris.juan@gmail.com>
2022-03-24 15:29:47 -0500

    fix(ci): vite config test

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,27 @@
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '',
-  root: './',
+  base: '.',
+  root: 'src',
+  resolve: {
+    alias: {
+      '~': resolve(__dirname, './src'),
+    }
+  },
   build: {
     emptyOutDir: true,
-    outDir: "build"
+    outDir: "../build",
+    sourcemap: true,
+    // rollupOptions: {
+    //   input: {
+    //     login: resolve(__dirname, 'pages/**/*.tsx'),
+    //   },
+    //   external: ['react', 'react-dom', 'vite', 'vite-tsconfig-paths'],
+    // }
   },
   plugins: [react(), tsconfigPaths()]
 })
