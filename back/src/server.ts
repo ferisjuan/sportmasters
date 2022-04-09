624e5f5b5b6df4d6a3ab229536d13c6cda5a74cf
Juan Feris <feris.juan@gmail.com>
2022-04-09 15:35:03 -0500

    feat(be): add apollo server

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,1 +1,31 @@
-console.log('hello world');
+// @vendors
+import "reflect-metadata"
+import { ApolloServer } from "apollo-server";
+import { Arg, buildSchema, Field, ID, ObjectType, Query, Resolver } from "type-graphql";
+
+@ObjectType()
+export class Player {
+    @Field(() => ID)
+    name: string = ''
+}
+
+@Resolver(Player)
+export class PlayersResolver {
+    @Query(() => [Player])
+    players(): Player[] {
+        return [
+            { name: 'John' },
+            { name: 'Jane' },
+        ]
+    }
+}
+
+const schema = await buildSchema({
+    resolvers: [PlayersResolver],
+})
+
+const server = new ApolloServer({
+    schema,
+})
+
+server.listen(process.env.PORT || 4000).then(({ url }) => { console.log(`🚀 Server ready at ${url}`) })
