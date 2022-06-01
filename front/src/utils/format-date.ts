fb3af4de163555b6c34964beed29c7077e27c48f
Juan Feris <feris.juan@gmail.com>
2022-06-01 10:21:47 -0500

    feat(fe): add luxon and date parser

--- front/src/utils/format-date.ts
+++ front/src/utils/format-date.ts
@@ -1,7 +1,14 @@
+import { DateTime } from 'luxon'
 export const formatDate = (seconds: number): string => {
     const date = new Date(seconds)
     const year = date.getFullYear()
     const month = date.getMonth()
     const day = date.getDate()
     return `${day}/${month}/${year}`
+}
+
+export const getDateMedWeekDay = (date: string): string => {
+    const dt = DateTime.fromISO(date)
+
+    return dt.toLocaleString()
 }
