cce56353287672b4ae449a919fec6e519278f525
Alejandro Olivera <aienma325@gmail.com>
2022-06-01 19:45:54 -0500

    feat(fe): solved auth

--- front/src/components/auth/auth.tsx
+++ front/src/components/auth/auth.tsx
@@ -1,4 +1,5 @@
 // @vendors
+import { observer } from 'mobx-react-lite'
 import { useLocation, Navigate } from 'react-router-dom'
 
 // @constants
@@ -10,7 +11,7 @@ import { IChildren } from '~/interfaces'
 // @hooks
 import { useAuthState } from './auth.hook'
 
-export const AuthenticatedRoute = ({ children }: IChildren): JSX.Element => {
+export const AuthenticatedRoute: React.FC<IChildren> = observer(({ children }) => {
     const { isAuthenticated } = useAuthState()
 
     const location = useLocation()
@@ -18,4 +19,4 @@ export const AuthenticatedRoute = ({ children }: IChildren): JSX.Element => {
     if (!isAuthenticated) return <Navigate to={`../${ROUTES.login}`} state={{ from: location }} />
 
     return children
-}
+})
