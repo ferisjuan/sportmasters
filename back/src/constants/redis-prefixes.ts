e3e6bc745f69e97e25015a2546bd472e499643d1
Alejandro Olivera <aienma325@gmail.com>
2022-06-06 15:40:18 -0500

    feat(fe): delete BE and .github

--- back/src/constants/redis-prefixes.ts
+++
@@ -1,2 +0,0 @@
-export const CONFIRM_USER_PREFIX = 'user-confirmation:'
-export const FORGOT_PASSWORD_PREFIX = 'forgot-password:'
--- back/src/modules/user/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './ChangePassword'
-export * from './ConfirmUser'
-export * from './ForgotPassword'
-export * from './Logout'
-export * from './Signin'
-export * from './signup'
