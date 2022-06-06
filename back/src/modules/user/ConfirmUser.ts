cdd7362f38e36cef05bc7a19583602a7246c7817
Alejandro Olivera <aienma325@gmail.com>
2022-06-06 14:03:36 -0500

    feat(be): change files

--- back/src/modules/user/ConfirmUser.ts
+++ back/src/modules/user/ConfirmUser.ts
@@ -1,22 +1,22 @@
 // @vendors
-import 'reflect-metadata'
+import "reflect-metadata"
 import { Arg, Ctx, Mutation, Resolver } from 'type-graphql'
 
-// @constants
-import { CONFIRM_USER_PREFIX } from '../../constants'
-
 // @context
-import { Context } from '../../context'
+import { Context } from '../../context';
 
 // @redis
-import { redis } from '../../redis'
+import { redis } from '../../redis';
 
 @Resolver()
 export class ConfirmUserResolver {
     @Mutation(() => Boolean)
-    async confirmUser(@Arg('token') token: string, @Ctx() ctx: Context): Promise<boolean> {
+    async confirmUser(
+        @Arg("token") token: string,
+        @Ctx() ctx: Context
+    ): Promise<boolean> {
         try {
-            const userId = await redis.get(`${CONFIRM_USER_PREFIX}${token}`)
+            const userId = await redis.get(token)
 
             if (!userId) return false
 
--- back/src/modules/user/signup.ts
+++ back/src/modules/user/signup.ts
@@ -1,29 +1,34 @@
 // @vendors
-import 'reflect-metadata'
+import "reflect-metadata"
 import { Arg, Ctx, Mutation, Resolver } from 'type-graphql'
 import bcrypt from 'bcryptjs'
-import { SignupInput } from './signup/Signup.input'
+import { SignupInput } from './signup/Signup.input';
 
 // @context
-import { Context } from '../../context'
+import { Context } from '../../context';
 
 // @generated
-import { User } from '../../generated/typegraphql-prisma.ts'
+import { User } from '../../generated/typegraphql-prisma.ts';
 
 // @utils
 import { createConfirmationUrl, sendMail } from '../../utils'
-import { HASH_SALT } from '../../constants/encryptation'
+import { HASH_SALT } from '../../constants/encryptation';
 
 @Resolver(User)
 export class SignupResolver {
     @Mutation(() => User)
-    async signup(
-        @Arg('data')
-        { acceptsPrivacyPolicy, acceptsTermsOfService, email, firstName, lastName, password, schoolEmail }: SignupInput,
-        @Ctx() ctx: Context,
-    ): Promise<Partial<User> | null> {
+    async signup(@Arg("data") {
+            acceptsPrivacyPolicy,
+            acceptsTermsOfService,
+            email,
+            firstName,
+            lastName,
+            password,
+            schoolEmail,
+        }: SignupInput,
+        @Ctx() ctx: Context): Promise<Partial<User> | null> {
         try {
-            const hashedPassword = await bcrypt.hash(password, HASH_SALT)
+            const hashedPassword = await bcrypt.hash(password, HASH_SALT);
 
             const user = await ctx.prisma.user.create({
                 data: {
@@ -33,12 +38,8 @@ export class SignupResolver {
                     firstName,
                     lastName,
                     password: hashedPassword,
-                    school: {
-                        connect: {
-                            email: schoolEmail,
-                        },
-                    },
-                },
+                    schoolEmail,
+                }
             })
 
             await sendMail(user.email, await createConfirmationUrl(user.id))
