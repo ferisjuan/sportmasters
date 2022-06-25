dec5a83f9c7ab0320bb675eeb98f0fd67f122d59
Juan Feris <feris.juan@gmail.com>
2022-06-25 14:30:44 -0500

    fix(ci): remove unused

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const express = require('express')
-const helmet = require('helmet')
 const app = express()
 
 const port = process.env.PORT || 3000
@@ -18,17 +17,6 @@ app.get('*', (req, res) => {
 app.all('*', (req, res, next) => {
     next(new AppError(`The requested URL ${req.originalUrl} could not be found`, 404))
 })
-
-app.use(
-    helmet.contentSecurityPolicy({
-        directives: {
-            defaultSrc: ["'self'", '/favicon.ico'],
-            fontSrc: ["'self'", 'fonts.googleapis.com', 'fonts.gstatic.com', 'res.cloudinary.com/'],
-            baseUri: ["'self'"],
-        },
-        reportOnly: true,
-    }),
-)
 
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`)
